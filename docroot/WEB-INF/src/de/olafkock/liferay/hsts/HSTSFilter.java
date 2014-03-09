dda4cc6e6c63095d663d38f34acf67831a465226
Olaf Kock <olaf.kock@liferay.com>
2014-03-09 15:30:24 +0100

    Security Manager enabled and Logging introduced

--- docroot/WEB-INF/src/de/olafkock/liferay/hsts/HSTSFilter.java
+++ docroot/WEB-INF/src/de/olafkock/liferay/hsts/HSTSFilter.java
@@ -1,5 +1,7 @@
 package de.olafkock.liferay.hsts;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.PropsUtil;
 
 import java.io.IOException;
@@ -14,35 +16,38 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HSTSFilter implements Filter {
 
-//	@Override
+	@Override
 	public void destroy() {
 	}
 
-//	@Override
+	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain filter) throws IOException, ServletException {
 		// run only if request comes through https
 		if(request.isSecure() && maxAge > 0) {
-//			System.out.println("request is secure - add HSTS Header");
 			try {
 				((HttpServletResponse)response).addHeader("Strict-Transport-Security", "max-age=" + maxAge);
 			} catch (Exception ignore) {
-//				System.err.println("ignored exception in HSTS Filter. No header added");
+				_log.error("ignored exception in HSTS Filter. No header added. (" + 
+						ignore.getClass().getName() + " " + ignore.getMessage() + ")");
 			}
 		} else {
-//			System.out.println("request is insecure - no HSTS Header added");
+			if(_log.isDebugEnabled())
+				_log.debug("request is insecure - no HSTS Header added");
 		}
 		filter.doFilter(request, response);
 	}
 
-//	@Override
+	@Override
 	public void init(FilterConfig config) throws ServletException {
 		maxAge = Long.valueOf(config.getInitParameter("max-age"));
 		if(PropsUtil.contains("hsts-max-age")) {
 			maxAge = Long.valueOf(PropsUtil.get("hsts-max-age"));
 		}
-//		System.out.println("HSTS-Filter configured for max-age " + maxAge);
+		if(_log.isDebugEnabled())
+			_log.debug("HSTS-Filter configured for max-age " + maxAge);
 	}
 
 	long maxAge = 0L;
+	private static Log _log = LogFactoryUtil.getLog(HSTSFilter.class);
 }
